90e1f341368df7d95041dc83f8027690ce214267
mahmoudomarus <mahmoudomarus@gmail.com>
2025-08-12 16:11:03 -0700

    fix: Update sandbox to use v1.0 tag instead of digest
    - Created mahmoudomarus/krib-sandbox:v1.0 tag in Docker Hub
    - Updated backend config to use v1.0 tag for compatibility with Daytona
    - Snapshot name remains 'krib' as configured in Daytona dashboard

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -221,9 +221,9 @@ class Configuration:
     STRIPE_PRODUCT_ID_PROD: str = 'prod_SCl7AQ2C8kK1CD'
     STRIPE_PRODUCT_ID_STAGING: str = 'prod_SCgIj3G7yPOAWY'
     
-    # Sandbox configuration - using custom Docker Hub image
-    SANDBOX_IMAGE_NAME = "mahmoudomarus/krib-sandbox:latest"
-    SANDBOX_SNAPSHOT_NAME = "mahmoudomarus/krib-sandbox:latest"
+    # Sandbox configuration - using custom Docker Hub image with v1.0 tag
+    SANDBOX_IMAGE_NAME = "mahmoudomarus/krib-sandbox:v1.0"
+    SANDBOX_SNAPSHOT_NAME = "krib"
     SANDBOX_ENTRYPOINT = "/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf"
 
     # LangFuse configuration
